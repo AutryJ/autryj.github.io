3354244b9a03e796d45c72e1065969da02fdd2dd
AutryJ <55469892+AutryJ@users.noreply.github.com>
2019-11-11 22:12:11 -0800

    commit to start json page

---
+++ lesson9/js/prophets.js
@@ -0,0 +1,35 @@
+const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
+const cardContainer = document.querySelector("div.cards");
+fetch(requestURL)
+    .then(response=>{return response.json()})
+    .then(json=>{
+        const prophets = json["prophets"];
+        prophets.forEach(prophet=>{
+
+            let card = document.createElement("section");
+            
+            
+            let h2 = document.createElement("h2");
+            h2.textContent = '${prophet.name} ${prophet.lastname}';
+           
+            
+            let p = document.createElement("p");
+            p.innerHTML = 'Date of Birth: ${prophet.birthdate} <br> Place of Birth: ${prophet.birthplace}';
+           
+            
+
+            let image = document.createElement("img");
+            image.setAttribute("src", prophet.imageurl);
+            image.setAttribute("alt", 'Picture of ${prophet.name} ${prophet.lastname} - ${prophet.order}');
+           
+           
+            card.appendChild(h2);
+            card.appendChild(p);
+            card.appendChild(image);
+            
+            
+            cardContainer.appendChild(card);
+        });
+    });
+
+  
