420b05cb669ba69307125cf32b16124e34dca4fe
AutryJ <55469892+AutryJ@users.noreply.github.com>
2019-11-14 22:07:00 -0800

    commit to start homepage

---
+++ lesson9/js/java.js
@@ -0,0 +1,35 @@
+const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+const cardContainer = document.querySelector("div.cards");
+fetch(requestURL)
+    .then(response=>{return response.json()})
+    .then(json=>{
+        const towns = json["towns"];
+        towns.forEach(town=>{
+
+            let card = document.createElement("section");
+            
+            
+            let h2 = document.createElement("h2");
+            h2.textContent = `${town.name}`;
+           
+            
+            let p = document.createElement("p");
+            p.innerHTML = `Motto: ${town.motto}<br>Founded: ${town.yearFounded}<br>Population: ${town.currentPopulation}<br>Avg. Rainfall: ${town.averageRainfall}`;
+           
+            
+
+            let image = document.createElement("img");
+            image.setAttribute("src", town.photo);
+            image.setAttribute("alt", `Picture of ${town.name}`);
+           
+           
+            card.appendChild(h2);
+            card.appendChild(p);
+            card.appendChild(image);
+            
+            
+            cardContainer.appendChild(card);
+        });
+    });
+
+  
